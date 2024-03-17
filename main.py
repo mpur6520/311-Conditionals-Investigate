c932dbd619572ba82320bbc108bc680cbb373e36
Maru Puran <144689560+mpur6520@users.noreply.github.com>
2024-03-17 20:03:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+#Maru Puran 
+#October 24 2023
+#Analyze and answer questions pertaining to code in order to gain an understanding of vocabulary and how the computer reads it as well as how to understand it better enough to explain using proper statements
+
+# Example Code
+
+number1 = int(input("Please enter a number")) #asks the user to enter a number and stores it in the variable number1 as a numerical value
+number2 = int(input("Please enter another number")) #asks the user to enter a number and stores it in the variable number2 as a numerical value
+
+if number1 > number2: #checks if number1 is bigger than number2
+  print("Number 1 is bigger than number 2") #print that is number1 is bigger than number2
+elif number2 > number1: #checks if number2 is bigger than number1
+  print("Number 2 is bigger than number 1") #print that number2 is bigger than number1
+else: #if both conditionals are false
+  print("Both numbers are the same") #print the numbers are the same or equal
+
+
+# What line does selection start on?
+  # Answer: It starts on line 10 with the if statement.
+
+# How many selection statements are there in the code?
+  # Answer: There are three selection statements in the code; if, else if, and else.
+
+# How many conditions are there in the code?
+  # Answer: There are 2 conditional statements in the code; one for if number1 is bigger than number2 (number1 > number2, and one for if number2 is bigger than number1 number 2 > number1).
+
+# What does the > operator mean?
+  # Answer: It means greater than; it's checking if the value to the left is greater than the one on the right.
+
+# Why are lines7, 9 and 11 indented?
+  # Answer: For a code to be run under something like an if statement in python, it has to be indented to let the program know this part of the code is run only when the statement is true.
+
+# What is the purpose of this program?
+  # Answer: The purpose of this program is to compare the values of numbers that the user is asked to input.
